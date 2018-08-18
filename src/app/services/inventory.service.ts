46c4b31379dca95664bb03f6ce45707ffdc0f8ad
Jorge Vergara <javebratt@gmail.com>
2018-08-17 20:18:35 -0500

    Adds the authentication guards functionality

--- src/app/services/inventory.service.ts
+++ src/app/services/inventory.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InventoryService {
+  userId: string;
+  constructor(private afAuth: AngularFireAuth) {
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        this.userId = user.uid;
+      }
+    });
+  }
 
-  constructor() { }
+  async getTeamId(): Promise<string> {
+    const userProfile: firebase.firestore.DocumentSnapshot = await firebase
+      .firestore()
+      .doc(`userProfile/${this.userId}`)
+      .get();
+
+    return userProfile.data().teamId;
+  }
 }
