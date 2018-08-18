dfa2723ff0d6d593ba6e0899ee457434f9d32a78
Jorge Vergara <javebratt@gmail.com>
2018-08-17 19:56:38 -0500

    Installs and Initializes Firebase/AngularFire2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,27 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { firebaseConfig } from './credentials';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/credentials.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBJTmt3LGBm5CTiO5DUZ4W3O5mTLphaoKY',
+  authDomain: 'javebratt-playground.firebaseapp.com',
+  databaseURL: 'https://javebratt-playground.firebaseio.com',
+  projectId: 'javebratt-playground',
+  storageBucket: 'javebratt-playground.appspot.com',
+  messagingSenderId: '369908572440',
+};
