35b8b8132552f089f013a29c3e5c31fa4f986933
Jorge Vergara <javebratt@gmail.com>
2018-08-18 11:02:14 -0500

    Adds the createTeamMember function.
    The function listens to the database and creates a new user for every team member added to the collection.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,29 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.createTeamMember = functions.firestore
+  .document(`teamProfile/{teamId}/teamMemberList/{newUserId}`)
+  .onCreate(async (snapshot, context) => {
+    const id: string = snapshot.data().id;
+    const email: string = snapshot.data().email;
+    const teamId: string = snapshot.data().teamId;
+
+    const newUser: admin.auth.UserRecord = await admin.auth().createUser({
+      uid: id,
+      email: email,
+      password: '123456789',
+    });
+
+    await admin
+      .firestore()
+      .doc(`userProfile/${id}`)
+      .set({
+        email: email,
+        id: id,
+        teamId: teamId,
+        teamAdmin: false,
+      });
+
+    return newUser;
+  });
