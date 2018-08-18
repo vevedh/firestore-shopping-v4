0da29b6c8c9ac32794b9d93a72a8ca130943a125
Jorge Vergara <javebratt@gmail.com>
2018-08-18 14:17:08 -0500

    Creates the inventory page

--- src/app/pages/inventory/inventory.page.ts
+++ src/app/pages/inventory/inventory.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { InventoryService } from '../../services/inventory.service';
+import { AlertController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { Grocery } from '../../models/grocery';
 
 @Component({
   selector: 'app-inventory',
@@ -6,10 +10,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inventory.page.scss'],
 })
 export class InventoryPage implements OnInit {
+  public groceryList: Observable<Grocery[]>;
 
-  constructor() { }
+  constructor(
+    private inventoryService: InventoryService,
+    private alertCtrl: AlertController
+  ) {}
 
   ngOnInit() {
+    this.inventoryService.getTeamId().then(teamId => {
+      this.groceryList = this.inventoryService.getGroceryList(teamId).valueChanges();
+    });
   }
 
+  async addGrocery(groceryId: string, teamId: string): Promise<void> {
+    const prompt = await this.alertCtrl.create({
+      message: 'How many are you adding?',
+      inputs: [
+        {
+          name: 'quantity',
+          placeholder: '0',
+          type: 'number',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          },
+        },
+        {
+          text: 'Add',
+          handler: data => {
+            const quantity: number = parseFloat(data.quantity);
+            this.inventoryService.addGroceryQuantity(groceryId, quantity, teamId);
+          },
+        },
+      ],
+    });
+    prompt.present();
+  }
+
+  async removeGrocery(groceryId: string, teamId: string): Promise<void> {
+    const prompt = await this.alertCtrl.create({
+      message: 'How many are you taking out?',
+      inputs: [
+        {
+          name: 'quantity',
+          placeholder: '0',
+          type: 'number',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          },
+        },
+        {
+          text: 'Take Out',
+          handler: data => {
+            const quantity: number = parseFloat(data.quantity);
+            this.inventoryService.removeGroceryQuantity(groceryId, quantity, teamId);
+          },
+        },
+      ],
+    });
+    prompt.present();
+  }
 }
--- src/app/services/inventory.service.ts
+++ src/app/services/inventory.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/app';
 import { AngularFireAuth } from 'angularfire2/auth';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument,
+} from 'angularfire2/firestore';
+import { Grocery } from '../models/grocery';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InventoryService {
   userId: string;
-  constructor(private afAuth: AngularFireAuth) {
+  constructor(private afAuth: AngularFireAuth, private fireStore: AngularFirestore) {
     this.afAuth.authState.subscribe(user => {
       if (user) {
         this.userId = user.uid;
@@ -22,5 +28,41 @@ export class InventoryService {
       .get();
 
     return userProfile.data().teamId;
+  }
+
+  getGroceryList(teamId: string): AngularFirestoreCollection<Grocery> {
+    return this.fireStore.collection<Grocery>(`/teamProfile/${teamId}/groceryList`, ref =>
+      ref.orderBy('quantity')
+    );
+  }
+
+  addGroceryQuantity(groceryId: string, quantity: number, teamId: string): Promise<void> {
+    const groceryRef: firebase.firestore.DocumentReference = this.fireStore.doc(
+      `/teamProfile/${teamId}/groceryList/${groceryId}`
+    ).ref;
+
+    return this.fireStore.firestore.runTransaction(transaction => {
+      return transaction.get(groceryRef).then(groceryDoc => {
+        const newQuantity: number = groceryDoc.data().quantity + quantity;
+        transaction.update(groceryRef, { quantity: newQuantity });
+      });
+    });
+  }
+
+  removeGroceryQuantity(
+    groceryId: string,
+    quantity: number,
+    teamId: string
+  ): Promise<void> {
+    const groceryRef = this.fireStore.doc(
+      `/teamProfile/${teamId}/groceryList/${groceryId}`
+    ).ref;
+
+    return this.fireStore.firestore.runTransaction(transaction => {
+      return transaction.get(groceryRef).then(groceryDoc => {
+        const newQuantity: number = groceryDoc.data().quantity - quantity;
+        transaction.update(groceryRef, { quantity: newQuantity });
+      });
+    });
   }
 }
